c9fb3a5a3aa4de82017c221304cb6d70a16fc928
Melvin Ensellem <ensellem.m@gmail.com>
2026-03-12 09:39:56 +0100

    Add ButtonGroup component and Separator, update documentation and metadata

--- content/components/_meta.js
+++ content/components/_meta.js
@@ -1,6 +1,7 @@
 export default {
   '-- action': { type: 'separator', title: 'Action' },
   button: 'Button',
+  'button-group': 'Button Group',
   '-- form': { type: 'separator', title: 'Form' },
   input: 'Input',
   field: 'Field',
